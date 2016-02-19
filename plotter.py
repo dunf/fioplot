f9cd3967f310ff69824654ecf30858c4d3621ca1
md <mihkal@dunfjeld.no>
2016-02-19 18:35:07 +0100

    Added fio script & the todays work on plotting script

--- plotter.py
+++ plotter.py
@@ -1,27 +1,72 @@
+#!/usr/bin/python3
+
 import os
 import sys
-import csv
+import json
+import argparse
+import bs4
 
 from matplotlib import pyplot as plt
 import numpy as np
 
 
-help(pyplot)
+class Args(object):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--directory", help="Directory where JSON files are located...",
+        default=None)
+    args = parser.parse_args()
+    DIR = args.directory
+
 
 class Plotter(object):
-    def __init__(self):
-        print("Plotter constructor")
+    args = Args()
+    results = {}
+    test_type = { 'read': 'read', 'write': 'write', 'mixed': 'mixed' }
 
-    def __del__(self):
-        print("Plotter destructor")
+    def read_files(self):
+        for fil in os.listdir(self.args.DIR):
+            with open(os.path.realpath(os.path.join(self.args.DIR, fil))) as file:
+                j_obj = json.load(file)
+                jobname = j_obj['jobs'][0]['jobname']
+                iops_list =  []
+                jobs = len(j_obj['jobs'])
+                read_or_write = j_obj['jobs'][0]['read']['io_bytes']
+
+                if read_or_write == 0:
+                    read_or_write = 'write'
+                else:
+                    read_or_write = 'read'
+                total_iops = 0
+                for job in range(jobs):
+                    iops = j_obj['jobs'][job][read_or_write]['iops']
+                    iops_list.append(iops)
+                    total_iops += iops
+                self.results.update({jobname: total_iops})
+       # print(self.results)
+
+    def plot(self):
+        #plt.title('tittei')
+        plt.xlabel('GROUP')
+        plt.ylabel('IOPS')
+        index = 0
+        for key, value in sorted(self.results.items()):
+            plt.bar(index, value, color='y')
+            print(key, value)
+            index += 1
 
 
+        plt.show()
 
+def usage():
+    if len(sys.argv) == 0:
+        print("Usage: ", sys.argv[0], " -d", " path to directory where JSON files are located...")
 
 def main():
-    pass
-    pass
-
+    usage()
+    p = Plotter()
+    p.read_files()
+    p.plot()
 
 if __name__ == "__main__":
     main()
+
---
+++ run_fio.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+# fio.bash /my_test_Dir_or_Device hw testname
+
+# all data is stored in a /data directory which should exist
+
+if [[ "$#" -ne 3 ]]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+
+if [[ ! $1 =~ ^/.+ ]]; then
+    echo "Please provide FULL PATH directory (where REALLYBIGFILE104 exists if this is a dir and not device)"
+    exit 1
+fi
+
+if [[ ! -d /data ]]; then
+    echo "/data does not exist!"
+    exit 1
+fi
+
+if [[ ! -e /usr/bin/fio ]]; then
+    echo "can't find exec /usr/bin/fio"
+    exit 1
+fi
+
+if [[ "$EUID" -ne 0 ]]
+  then echo "Please run as root"
+  exit 1
+fi
+
+dirordev=$1
+hw=$2
+testname=$3
+runtime=120   # in seconds
+ramptime=60   # in seconds
+#filesize=32G  # filesize for fallocate
+
+temp_dir=/data/${hw}/${testname}/$(awk -v p=$$ 'BEGIN { srand(); s = rand(); \
+   sub(/^0./, "", s); printf("%X_%X", p, s) }')
+mkdir -p "$temp_dir" || { echo '!! unable to create a tempdir' >&2; \
+   temp_dir=; exit 1; }
+
+if [[ -d $dirordev ]]; then
+  cd $dirordev
+  # create huge file to test against
+  filename="$(pwd)/REALLYBIGFILE104"
+  if [[ ! -f $filename ]]; then
+    echo "$filename does not exist!"
+    exit 1
+  fi
+# fallocate -l $filesize $filename
+elif [[ -b $dirordev ]]; then
+  filename=$dirordev
+else
+  echo "$dirordev is not directory or device"
+  exit
+fi
+
+
+cd $temp_dir
+
+
+echo "Sequential read bs=256K, no ramp_time"
+jobs=1
+depth=16
+name=fsseqR${jobs}-${depth}
+sync && echo 3 > /proc/sys/vm/drop_caches
+fio --filename=$filename --direct=1 --rw=read --refill_buffers --ioengine=libaio \
+--bs=256k --iodepth=$depth --numjobs=$jobs --runtime=$runtime --log_avg_msec=1000 \
+--write_iops_log=${name}-iopslog --write_bw_log=${name}-bwlog \
+--name=$name > $temp_dir/$name
+
+
+echo "Sequential write bs=256K, no ramp_time"
+jobs=1
+depth=16
+name=fsseqW${jobs}-${depth}
+sync && echo 3 > /proc/sys/vm/drop_caches
+fio --filename=$filename --direct=1 --rw=write --refill_buffers --ioengine=libaio \
+--bs=256k --iodepth=$depth --numjobs=$jobs --runtime=$runtime --log_avg_msec=1000 \
+--write_iops_log=${name}-iopslog --write_bw_log=${name}-bwlog \
+--name=$name > $temp_dir/$name
+
+
+echo "Random read"
+for jobs in 1 16
+  do
+    for depth in 1 16 32 64
+      do
+        echo " Doing $jobs jobs with depth $depth"
+        name=fsrandR${jobs}-${depth}
+        sync && echo 3 > /proc/sys/vm/drop_caches
+        fio --filename=$filename --direct=1 --rw=randread --refill_buffers --norandommap \
+        --randrepeat=0 --ioengine=libaio --bs=4k --iodepth=$depth \
+        --numjobs=$jobs --runtime=$runtime --log_avg_msec=1000 --write_iops_log=${name}-iopslog \
+        --write_bw_log=${name}-bwlog \
+        --ramp_time=$ramptime --name=$name > $temp_dir/$name
+    done
+done
+
+
+echo "Random write"
+for jobs in 1 16
+  do
+    for depth in 1 16 32 64
+      do
+        echo " Doing $jobs jobs with depth $depth"
+        name=fsrandW${jobs}-${depth}
+        sync && echo 3 > /proc/sys/vm/drop_caches
+        fio --filename=$filename --direct=1 --rw=randwrite --refill_buffers --norandommap \
+        --randrepeat=0 --ioengine=libaio --bs=4k --iodepth=$depth \
+        --numjobs=$jobs --runtime=$runtime --log_avg_msec=1000 --write_iops_log=${name}-iopslog \
+        --write_bw_log=${name}-bwlog \
+        --ramp_time=$ramptime --name=$name > $temp_dir/$name
+     done
+done
+
+
+# Combined workload from Storage Review
+# http://www.storagereview.com/fio_flexible_i_o_tester_synthetic_benchmark
+echo "Mixed 70/30 random read and write with 8K block size"
+jobs=16
+depth=16
+name=fsmixedRW7030${jobs}-${depth}
+sync && echo 3 > /proc/sys/vm/drop_caches
+fio --filename=$filename --direct=1 --rw=randrw --refill_buffers --norandommap \
+--randrepeat=0 --ioengine=libaio --bs=8k --rwmixread=70 --iodepth=$depth \
+--unified_rw_reporting=1 --numjobs=$jobs --runtime=$runtime --log_avg_msec=1000 \
+--write_iops_log=${name}-iopslog --write_bw_log=${name}-bwlog \
+--ramp_time=$ramptime --name=$name > $temp_dir/$name
+
+exit 0
