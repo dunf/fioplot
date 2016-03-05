512f2765bd015dada0ffc8fc627a5c0a77556dc2
md <mihkal@dunfjeld.no>
2016-03-05 16:10:53 +0100

    Release Candidate 2

--- plotter.py
+++ plotter.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 
-# This is a rewrite of Erik Hjelmås' plotsintex script
-# Usage: ./plotter -d DIRECTORY
+# This is a fork of Erik Hjelmås' plotsintex script.
+# Usage: ./plotter -t DIRECTORY
+# The directories needs to follow this structure:
+# DIRECTORY/SomeConfiguration/RandomGeneratedFolder/fiofiles
+
+# Author: Mihkal Dunfjeld
+
+#
 
 import os
 import argparse
 from matplotlib import pyplot as plt
 import numpy as np
-from numpy import *
+#from numpy import *
 
-test_type_desc = {
+test_type_desc = {      # Dict used for titles in diagrams
     'fsseqR1-16': 'Seq Read bs=256K, 1 job, IOdepth 16',
     'fsseqW1-16': 'Seq Write bs=256K 1 job, IOdepth 16',
 	'fsrandR1-1': 'Random Read 1 jobs, IOdepth 1',
@@ -31,10 +37,10 @@ test_type_desc = {
     'fsmixedRW703016-16': 'Mixed RW 70/30 bs=8K, 16 jobs, IOdepth 16',
 }
 
-test_type = [       # Array of tuples where the digit is the number of jobs for each test
-    ('fsseqR1-16', 1),
-    ('fsseqW1-16', 1),
-    ('fsrandR1-1', 1),
+test_type = [               # Array of tuples where the digit is the number of jobs for each test
+    ('fsseqR1-16', 1),      # When adding a new test, the naming convention is as follows:
+    ('fsseqW1-16', 1),      # fs + rand/seq/mixed + (R)ead/(W)rite + number of jobs + - + IO depth
+    ('fsrandR1-1', 1),      #
     ('fsrandR1-16', 1),
 	('fsrandR1-32', 1),
 	('fsrandR1-64', 1),
@@ -46,21 +52,17 @@ test_type = [       # Array of tuples where the digit is the number of jobs for 
 	('fsrandW1-16', 1),
 	('fsrandW1-32', 1),
 	('fsrandW1-64', 1),
-	('fsrandW16-1', 1),
+	('fsrandW16-1', 16),
 	('fsrandW16-16', 16),
 	('fsrandW16-32', 16),
 	('fsrandW16-64', 16),
     ('fsmixedRW703016-16', 16),
 ]
 
-# Description of the test config
+# Description of the test config.
 test_config = [
-   # 'test1',
-    #'test3',
-    'ssdjournal',
-    #'jobs_test',
-    #'qd_test',
-  #  'qd_test_onBlockDev'
+    'Journal on SSD filesystem',
+    'Journal on SSD blockdevice'
 ]
 
 
@@ -75,7 +77,7 @@ class Args(object):
 
 class Plotter(object):
     args = Args()
-
+    test_data = []  # (file, config, stdDev, raw iops, fio iops)
     def do_stuff(self):
         for test in test_type:
             num_jobs = test[1]
@@ -84,9 +86,10 @@ class Plotter(object):
             std_dev = []
             fio_means = []
             names = []
-            means = zeros(len(test_config))            # No bueno
-            #std_dev = zeros(len(test_config))          # No bueno
-            #fio_means = zeros(len(test_config))        # No bueno
+
+          #  means = zeros(len(test_config))            # No bueno
+          #  std_dev = zeros(len(test_config))          # No bueno
+          #  fio_means = zeros(len(test_config))        # No bueno
 
             for conf in test_config:
                 print("Processing test:", conf, "---- File: ", test[0])
@@ -95,39 +98,36 @@ class Plotter(object):
                     key=lambda last_change: os.path.getctime(os.path.join(my_path, last_change)))
                 newest = newest_tmp[-1]
                 dir_with_tests = os.path.realpath(self.args.DIR)
-                filename = os.path.join(dir_with_tests, conf, newest, test[0] + '-iopslog_iops.log')
-                filenameX = os.path.join(dir_with_tests, conf, newest, test[0])
+                raw_file = os.path.join(dir_with_tests, conf, newest, test[0] + '-iopslog_iops.log')
+                fio_output = os.path.join(dir_with_tests, conf, newest, test[0])
                 time = []
                 values = []
-                present = False
+                file_exists = False
                 try:
-                    with open(filename) as file:
+                    with open(raw_file) as file:
                         for line in file:
                             line_data = line.split(',') # [0] is time, [1] is IOPS
                             time.append(int(line_data[0]))
                             values.append(int(line_data[1]))
-                    present = True
+                    file_exists = True
+                    time.append(0)
                 except FileNotFoundError:
-                    print("Error!", filename, "not found...")
+                    print("Error!", raw_file, "not found...")
                     continue
 #
 #-------------------- NEW -------------------------------------
-                all_jobs = []
-                last_element = len(time)-1
+                std_dev.append(np.std(values))
                 i = 0
-                avg_sum = 0
+                raw_iops_sum = 0
                 for job in range(num_jobs):
                     job_values = []
-                    while (time[i] <= time[i+1]) and (i < last_element-1):
+                    while (time[i] <= time[i+1]): #and (i < last_element-1):
                         job_values.append(values[i])
                         i += 1
-                    avg_sum += np.mean(job_values)
-
-                    #all_jobs.append(np.mean(per_job))
+                    raw_iops_sum += np.mean(job_values)
                     i += 1
-                print("Sum avg IOPS: ", avg_sum)
 
-# ------------------------------ # ERIKS LØSNING ----------------------------------------
+# ------------------------------ # ERIKS LØSNING --------------------------------------------------
 #                i = 0
 #                tmp = zeros(130)
 #                last_element = len(time)-1
@@ -141,54 +141,47 @@ class Plotter(object):
 #                    i += 1
 #                if i == last_element:
 #                    tmp[k+1] += values[i]
-
-# -----------------------------------------------------------------------
-              #  print(tmp)
-        #        means.append(np.mean(all_jobs))
-
-               # print("AVG: ", tmp.mean())
-#                print("StdDev: ", tmp.std())
-#                print("AVG: ", np.mean(tmp))
-            #    std_dev[index] = np.std(tmp)
-                means[index] = avg_sum         #tmp.mean()
-              #  print("StdDev: ", np.std(tmp))
+                # print("AVG: ", tmp.mean())
+# -------------------------------------------------------------------------------------------------
                 try:
-                    with open(filenameX) as file:
-                        total = 0
+                    with open(fio_output) as file:
+                        iops_sum = 0
                         for line in file:
                             if 'iops' in line:
                                 iops = int(line.split('iops=')[1].split(',')[0])
-                                total += iops
-                    present = True
+                                iops_sum += iops
+                    file_exists = True
                 except FileNotFoundError:
-                    print("Error! File ", filenameX, "not found...")
+                    print("Error! File ", fio_output, "not found...")
                     continue
-                print("FIO IOPS: ", total)
-        #        fio_means[index] = total
-                fio_means.append(total)
+                print("RAW IOPS: ", raw_iops_sum)
+                print("FIO IOPS: ", iops_sum)
+                print("Standard deviation: ", std_dev[index])
+                means.append(raw_iops_sum)
+                fio_means.append(iops_sum)
                 names.append(conf)
                 index += 1
             ind = np.arange(len(means))
             width = 0.3
-            rects1 = plt.bar(ind, means, width, color='gray', #yerr=std_dev,
-                             error_kw=dict(ecolor='black'))
-            rects2 = plt.bar(ind+width, fio_means, width, color='brown')
+            plt.bar(ind, means, width, color='gray', yerr=std_dev, error_kw=dict(ecolor='black'))
+            plt.bar(ind+width, fio_means, width, color='green')
             plt.ylabel('IOPS')
             plt.title(test_type_desc.get(test[0]))
             plt.xticks(ind+width/2, names, rotation=270)
-           # plt.text(ind+0.45, fio_means+50, fio_means, ha='center')
             fig = plt.gcf()
             fig.subplots_adjust(bottom=0.4)
             plt.savefig('/home/md/Dropbox/Cephios/fioplot/results_plotter/' + test[0] + '.pdf')
-            if present == True:
+            if file_exists is True:
                 print("File " + test[0] + ".pdf saved...")
-            print('-' * 30)
+            print('-' * 80)
             plt.close()
 
 
 def main():
     p = Plotter()
-    p.do_stuff()
+    if p.args.DIR:
+        p.do_stuff()
+
 
 if __name__ == "__main__":
     main()
