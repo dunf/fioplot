6a399eda08fa6f6b35cc9cdfa6acb5a3e8391cd9
md <mihkal@dunfjeld.no>
2016-04-11 10:08:30 +0200

    Test config fil

--- test_config.py
+++ test_config.py
@@ -2,11 +2,25 @@
 
 class Config(object):
     configurations = [
-        'JournalonSSDfilesystem',
+#        'JournalonSSDfilesystem',
         #    'JournalonSSDblockdevice',
-        'BaseTest_SAS_disks',
-        'SAS_disks_MaxSync_1',
-        'SAS_disks_MaxSync_10',
-        'MaxSync_10_Run_2',
+#        'G6_run_1',
+ #       'G6_run_2',
+     #   'run_2_600sec',
+      #  'run2',
+      #  'titlap_3',
+#        'jobs_qd_testing_run_1',
+#        'MaxSync_5_run_1',
+        '1 sec',
+        '5 sec',
+#        'MaxSync_1_run_1',
+#        'MaxSync_10_run_1',
+#        'MaxSync_10_run_2',
+#        'MaxSync_10_run_3',
+        '10 sec',
+        #'HDD_ONLY_run_1',
+#        'HDD_ONLY_run_2',
+     #   'TEST',
+      #  'Fio27_test',
     ]
 
