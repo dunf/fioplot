00a0582bbdf74b8fd011291c31710bc4152b8e0d
md <mihkal@dunfjeld.no>
2016-05-03 22:34:00 +0200

    Prøver å gjøre scriptet mer fleksibelt

--- plotter.py
+++ plotter.py
@@ -3,8 +3,8 @@
 # This is a fork of Erik Hjelmås' plotsintex script.
 # Usage: ./plotter -t DIRECTORY
 # The directories needs to follow this structure:
-# DIRECTORY/SomeConfiguration/RandomGeneratedFolder/fiofiles
-# Add additional test configurations to test_config.py
+# DIRECTORY/SomeConfig/SomeFolder/fiofiles
+# Add additional test configurations to plotter_config.py
 
 # Author: Mihkal Dunfjeld
 # Repository: https://bitbucket.org/dunf/fioplot/src/
@@ -15,7 +15,6 @@ from matplotlib import pyplot as plt
 import numpy
 import plotter_config
 import matplotlib.patches as mpatches
-import sys
 import pwd
 
 
@@ -31,33 +30,68 @@ class Args(object):
     HIST_DIR = args.histogram
 
 
-class File(object):
-    def __init__(self, name, time, values):
-        self.name = name
-        self.values = values = []
-        self.time = time = []
-
 class Plotter(object):
     args = Args()
+    test_objects = []
     configs = plotter_config.Config.configurations
     test_type = plotter_config.Config.test_type
-    file_objs = []
 
     def read_files(self):
-    #    try:
         for conf in self.configs:
-            my_path = os.path.join(self.args.HIST_DIR, conf)
+            my_path = os.path.join(self.args.DIR, conf)
             newest_tmp = sorted(os.listdir(my_path),    # Only needed for compatibility with run_fio.sh
                 key=lambda last_change: os.path.getctime(os.path.join(my_path, last_change)))
             newest = newest_tmp[-1]
-            test_dir = os.path.realpath(self.args.HIST_DIR)
-            raw_file = os.path.join(test_dir, conf, newest + '-iopslog_iops.log')
-#        except Exception:
- #           print("Error! Config not found...")
+            test_dir = os.path.realpath(self.args.DIR)
+            for test in self.test_type:
+                raw_file = os.path.join(test_dir, conf, newest, test[0] + '-iopslog_iops.log')
+                fio_output = os.path.join(test_dir, conf, newest, test[0])
+                time = []
+                values = []
+                try:
+                    with open(fio_output) as file:
+                        print(fio_output)
+                        iops_sum = 0
+                        for line in file:
+                            if 'iops' in line:
+                                iops_sum += int(line.split('iops=')[1].split(',')[0])   # IOPS
+                except FileNotFoundError:
+                    print("Error! File ", fio_output, "not found...")
+                    continue
+                try:
+                    with open(raw_file) as file:
+                        print(raw_file)
+                        for line in file:
+                            line_data = line.split(',') # [0] is time, [1] is IOPS
+                            time.append(int(line_data[0]))
+                            values.append(int(line_data[1]))
+                except FileNotFoundError:
+                    print("Error!", raw_file, "not found...")
+                    continue
+                i = 0
+                raw_iops_avg = 0
+                for job in range(test[1]):  # Test[1] = number of jobs
+                    job_values = []
+                    while time[i] <= time[i+1]:
+                        job_values.append(values[i])
+                        i += 1
+                    job_values.append(values[i])
+                    raw_iops_avg += numpy.mean(job_values)
+                    i += 1
+                # Tuple containing test data are added to an array
+                self.test_objects.append((conf, test[0], raw_iops_avg, iops_sum))
 
-# devel
     def create_barchart(self):
-        for test in self.test_type:
+        fio_means = []
+        means = []
+        names = []
+        for test in self.test_objects:
+            print(test)
+
+
+
+    def old_create_barchart(self):
+        for test in configs.test_type:
             num_jobs = test[1]
             index = 0
             means = []
@@ -133,7 +167,6 @@ class Plotter(object):
                     continue
                 print(" " * 2, "FIO IOPS: ", iops_sum)
                 print(" " * 2, "RAW IOPS: ", raw_iops_avg)
-#                print(" " * 2, "Raw IOPS/120: ", temp)         # Eriks calculation method
                 print(" " * 2, "Standard deviation: ", std_dev[index])
                 means.append(raw_iops_avg)
                 fio_means.append(iops_sum)
@@ -177,30 +210,11 @@ class Plotter(object):
             plt.close()
 
 
-    def create_histogram(self):
-        for test in self.test_type:
-            time = []
-            values = []
-            dir_with_files = os.path.realpath(self.args.HIST_DIR)
-            newest_tmp = sorted(os.listdir(dir_with_files),
-                    key=lambda last_change: os.path.getctime(os.path.join(dir_with_files, last_change)))
-            newest = newest_tmp[-1]
-            raw_files = os.path.join(dir_with_files, newest, test[0] + '-iopslog_iops.log')
-            file_exists = False
-            try:
-                with open(raw_files) as file:
-                    for line in file:
-                        line_data = line.split(',') # [0] is time, [1] is IOPS
-                        time.append(int(line_data[0]))
-                        values.append(int(line_data[1]))
-            except FileNotFoundError:
-                print("Error! File ", file, "not found...")
-                continue
-
 
 def main():
     p = Plotter()
     if p.args.DIR:
+        p.read_files()
         p.create_barchart()
     elif p.args.HIST_DIR:
         pass
