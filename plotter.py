476c8a00db795a23f4794339e4563cd5fe0bd6c7
md <mihkal@dunfjeld.no>
2016-05-04 10:58:44 +0200

    Rydda litt i koden

--- plotter.py
+++ plotter.py
@@ -15,27 +15,20 @@ from matplotlib import pyplot as plt
 import numpy
 import plotter_config
 import matplotlib.patches as mpatches
-import sys
 import pwd
-
 
 
 class Args(object):
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--testdir", help="Generate barchart for all test configurations")
     parser.add_argument('-d', '--destination', help='Destination dir for output files', nargs=1)
-    parser.add_argument('-r', '--histogram', nargs=1, help='Dir in which you want to generate histograms')
+    parser.add_argument('-r', '--histogram', nargs=1,
+                        help='Dir in which you want to generate histograms')
     args = parser.parse_args()
     DIR = args.testdir
     DESTINATION = args.destination
     HIST_DIR = args.histogram
 
-
-class File(object):
-    def __init__(self, name, time, values):
-        self.name = name
-        self.values = values = []
-        self.time = time = []
 
 class Plotter(object):
     args = Args()
@@ -43,19 +36,6 @@ class Plotter(object):
     test_type = plotter_config.Config.test_type
     file_objs = []
 
-    def read_files(self):
-    #    try:
-        for conf in self.configs:
-            my_path = os.path.join(self.args.HIST_DIR, conf)
-            newest_tmp = sorted(os.listdir(my_path),    # Only needed for compatibility with run_fio.sh
-                key=lambda last_change: os.path.getctime(os.path.join(my_path, last_change)))
-            newest = newest_tmp[-1]
-            test_dir = os.path.realpath(self.args.HIST_DIR)
-            raw_file = os.path.join(test_dir, conf, newest + '-iopslog_iops.log')
-#        except Exception:
- #           print("Error! Config not found...")
-
-# hei
     def create_barchart(self):
         for test in self.test_type:
             num_jobs = test[1]
@@ -68,7 +48,7 @@ class Plotter(object):
             for conf in self.configs:
                 print("Test: ", test[2], "---- Configuration: ", conf)
                 my_path = os.path.join(self.args.DIR, conf)
-                newest_tmp = sorted(os.listdir(my_path),    # Needed for compatibility with run_fio.sh
+                newest_tmp = sorted(os.listdir(my_path), # Needed for compatibility with run_fio.sh
                     key=lambda last_change: os.path.getctime(os.path.join(my_path, last_change)))
                 newest = newest_tmp[-1]
                 dir_with_tests = os.path.realpath(self.args.DIR)
@@ -90,7 +70,6 @@ class Plotter(object):
                     continue
                 std_dev.append(numpy.std(values))
                 i = 0
-                temp = 0
                 raw_iops_avg = 0
                 for job in range(num_jobs):
                     job_values = []
@@ -99,27 +78,7 @@ class Plotter(object):
                         i += 1
                     job_values.append(values[i])
                     raw_iops_avg += numpy.mean(job_values)
-                    temp += sum(job_values)/120
                     i += 1
-                t1.append(temp)
-# ------------------------------ # ERIKS LÃ˜SNING --------------------------------------------------
-#                tmp = numpy.zeros(120)
-#                i = 0
-#                last_element = len(time)-1
-#                for j in range(num_jobs):
-#                    k = 0
-#                    while (time[i] <= time[i+1]) and (i < last_element-1):
-#                        tmp[k] += values[i]
-#                        k += 1
-#                        i += 1
-#                    tmp[k] += values[i]
-#                    i += 1
-#         ##       if i == last_element:
-#           #         tmp[k] += values[i]
-#            #    print("AVG: ", tmp.mean())
-#                erik_means[index] = tmp.mean()
-            #    erik_means.append(tmp.mean())
-# -------------------------------------------------------------------------------------------------
                 try:
                     with open(fio_output) as file:
                         iops_sum = 0
@@ -133,7 +92,6 @@ class Plotter(object):
                     continue
                 print(" " * 2, "FIO IOPS: ", iops_sum)
                 print(" " * 2, "RAW IOPS: ", raw_iops_avg)
-#                print(" " * 2, "Raw IOPS/120: ", temp)         # Eriks calculation method
                 print(" " * 2, "Standard deviation: ", std_dev[index])
                 means.append(raw_iops_avg)
                 fio_means.append(iops_sum)
@@ -144,70 +102,31 @@ class Plotter(object):
             rects1 = plt.bar(ind, means, width, color='gray', align='center', yerr=std_dev,
                 error_kw=dict(ecolor='black'))
             rects2 = plt.bar(ind+width, fio_means, width, color='green', align='center')
-#            plt.bar(ind+width+0.3, t1, width, color='red')     # Uncomment for Erik score
             plt.ylabel('IOPS')
             plt.title(test[2])
             plt.xticks(ind+width/2, names, rotation=270)
             plt.grid(True, axis='y')
-
-#           How to rotate values?
-#            for rect in rects2:
-#                height = rect.get_height()
-#                plt.text(rect.get_x() + rect.get_width()/2., 1.02*height,
-#                        '%d' % int(height),
-#                        ha='center', va='bottom' )
-
-
             green = mpatches.Patch(color='green', label='Standard output IOPS ')
             grey = mpatches.Patch(color='grey', label='Raw IOPS')
-#            red = mpatches.Patch(color='red', label='Raw IOPS/120')
             plt.legend(handles=[grey, green], bbox_to_anchor=(0.8, 0.1), ncol=2,
                 bbox_transform=plt.gcf().transFigure)
             fig = plt.gcf()
             fig.subplots_adjust(top=0.95)
             fig.subplots_adjust(bottom=0.4)
-#            plt.tight_layout()
-#            try:
-            plt.savefig('/home/' + pwd.getpwuid(os.getuid()).pw_name + '/Dropbox/Cephios/fioplot/results/' + test[0] + '.pdf')
-#            except FileNotFoundError:
-#                plt.savefig('/home/birger/Dropbox/Cephios/fioplot/results/' + test[0] + '.pdf')
+            plt.savefig('/home/' + pwd.getpwuid(os.getuid()).pw_name +
+                        '/Dropbox/Cephios/fioplot/results/' + test[0] + '.pdf')
             if file_exists is True:
                 print("File " + test[0] + ".pdf saved...")
             print('-' * 90)
             plt.close()
 
 
-    def create_histogram(self):
-        for test in self.test_type:
-            time = []
-            values = []
-            dir_with_files = os.path.realpath(self.args.HIST_DIR)
-            newest_tmp = sorted(os.listdir(dir_with_files),
-                    key=lambda last_change: os.path.getctime(os.path.join(dir_with_files, last_change)))
-            newest = newest_tmp[-1]
-            raw_files = os.path.join(dir_with_files, newest, test[0] + '-iopslog_iops.log')
-            file_exists = False
-            try:
-                with open(raw_files) as file:
-                    for line in file:
-                        line_data = line.split(',') # [0] is time, [1] is IOPS
-                        time.append(int(line_data[0]))
-                        values.append(int(line_data[1]))
-            except FileNotFoundError:
-                print("Error! File ", file, "not found...")
-                continue
-
-
 def main():
     p = Plotter()
     if p.args.DIR:
         p.create_barchart()
-    elif p.args.HIST_DIR:
-        pass
-      #  p.create_histogram()
     else:
         pass
-
 
 if __name__ == "__main__":
     main()
